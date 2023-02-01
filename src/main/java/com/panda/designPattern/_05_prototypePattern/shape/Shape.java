ee26443f10733e9217467ba8b6f6909ebaaa02f7
xiongbl <xiongbl8@chinaunicom.cn>
2023-02-01 14:04:51 +0800

    原型模式

---
+++ src/main/java/com/panda/designPattern/_05_prototypePattern/shape/Shape.java
@@ -0,0 +1,37 @@
+package com.panda.designPattern._05_prototypePattern.shape;
+
+public abstract class Shape implements Cloneable{
+    private int id;
+    protected String type;
+
+    public abstract void draw();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+
+    @Override
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
