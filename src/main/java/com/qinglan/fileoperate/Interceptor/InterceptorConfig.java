b1a14068ad6d3a8ef2a89a053808bbbb0892f0ae
xiongbl <xiongbl8@chinaunicom.cn>
2023-03-10 15:35:40 +0800

    拦截器

---
+++ src/main/java/com/qinglan/fileoperate/Interceptor/InterceptorConfig.java
@@ -0,0 +1,23 @@
+package com.qinglan.fileoperate.Interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    HandlerInterceptorDemo handlerInterceptorDemo;
+    @Autowired
+    HandlerInterceptorDemo1 handlerInterceptorDemo1;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(handlerInterceptorDemo).addPathPatterns("/**");
+        registry.addInterceptor(handlerInterceptorDemo1).addPathPatterns("/**");
+
+        WebMvcConfigurer.super.addInterceptors(registry);
+
+    }
+}
